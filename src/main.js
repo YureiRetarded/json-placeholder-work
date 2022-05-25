20c0b781ef463281e187b6d1fec6aeeb16d2afec
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-05-25 20:32:00 +0300

    installed corrected version bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/style.css'
 
 const app = createApp(App)
 app
     .use(router)
+    .use(BootstrapVue)
     .mount('#app')
