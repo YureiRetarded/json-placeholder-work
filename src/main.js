effd0a4653cbe8aaa30a59e32cc866f2ea9619a1
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-05-25 22:12:25 +0300

    ok, the previous version was working

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/style.css'
 
 const app = createApp(App)
-app
-    .use(router)
-    .use(BootstrapVue)
-    .mount('#app')
+app.use(router).mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.js'
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -5,6 +5,29 @@
     <div class="subtitle">
         Имитация сайта с станицами пользователей и их постами
     </div>
+    <div class="test__bp">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://picsum.photos/id/237/600/400" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://picsum.photos/id/239/600/400" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://picsum.photos/id/234/600/400" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>                   
+    </div>
 </template>
 <script>
 export default {
