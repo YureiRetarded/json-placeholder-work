efa355e52db975970089eb140034560b1b264a9c
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-05-27 17:20:27 +0300

    main page is updated

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,33 +1,33 @@
 <template>
+  <div class="container b1">
     <div class="title">
-        JSON P Test work
+      Work with JSON placeholder
     </div>
     <div class="subtitle">
-        Имитация сайта с станицами пользователей и их постами
-    </div>
-    <div class="test__bp">
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://picsum.photos/id/237/600/400" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://picsum.photos/id/239/600/400" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://picsum.photos/id/234/600/400" class="d-block w-100" alt="...">
+      Имитация сайта со страницами пользователей и их постами
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>                   
+  <div class="container b2">
+    <div class="main-text">
+      <p>Сайт полность создан на vue, используется 'vue router' и 'vuex', также используется 'bootstrap 5'</p>
+      <a class="btn btn-dark" href="https://github.com/YureiRetarded">My GitHub</a>
     </div>
+  </div>
+  <div class="container b3">
+    <div class="secondary-text">
+      <p> 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id libero urna. Phasellus tempus iaculis metus, a fermentum dolor ullamcorper non. In sit amet nisl varius, tincidunt quam sit amet, pharetra purus. Integer efficitur vulputate nibh, eu ornare arcu auctor eu. Donec auctor aliquam nibh, eu mattis ligula vestibulum sit amet. Donec pellentesque vel ex iaculis tincidunt. Donec laoreet sem enim, vel dictum nisl finibus sit amet. Praesent ultrices quam sit amet tortor dictum pellentesque. Praesent lobortis lobortis nisi.
+        Nulla pellentesque dui non lectus euismod, ut fermentum tellus rhoncus. Aliquam mi risus, mollis a tempus in, cursus at mauris. Nam scelerisque, enim ac congue scelerisque, orci ante tincidunt libero, id dignissim elit orci et nisi. Phasellus odio augue, faucibus quis fermentum vitae, accumsan non metus. Sed sagittis dictum mauris, in porta tellus venenatis at. Curabitur semper ex tellus, eget tempor tortor fringilla eget. Fusce magna ligula, ornare nec efficitur et, commodo quis velit. Nulla eget molestie nisi. Ut porta ac est nec eleifend. Ut lobortis ut neque sed rutrum. Curabitur id tincidunt elit, ac imperdiet orci.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus venenatis nibh vel erat ullamcorper faucibus. Duis ac porta mi, nec lacinia risus. Etiam est elit, interdum a leo ac, varius malesuada felis. Proin non est commodo, euismod diam in, fringilla ante. In mauris mi, finibus ac eleifend ut, accumsan quis metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus mattis luctus justo id ultrices. Suspendisse a ex eros. Cras et mi sed est euismod elementum.
+        Nam dignissim elit sit amet rutrum hendrerit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pharetra elit finibus quam dictum luctus. Phasellus eget augue malesuada, molestie felis eget, laoreet orci. Nullam nibh urna, viverra at iaculis sit amet, posuere vel felis. Ut imperdiet fringilla odio, quis cursus arcu dictum vel. Integer ligula ante, ultrices vitae nisi eget, malesuada pulvinar arcu. Aenean dignissim quis urna eu pellentesque. Curabitur congue dui et orci feugiat porttitor. Etiam finibus ante vel vulputate fermentum. Nulla auctor sapien nec libero dictum dignissim. Donec id lacus turpis.
+        Praesent finibus, elit vitae vestibulum maximus, ex dolor mattis ex, sit amet cursus orci augue vitae ligula. Aenean vitae velit sed nisi venenatis volutpat sit amet eget turpis. Praesent id lacus tellus. Nulla facilisi. Quisque non diam lacus. Nullam in maximus neque. Nunc nec nulla massa. Quisque maximus, felis congue iaculis pellentesque, felis urna tincidunt eros, hendrerit euismod eros leo vitae ipsum. Proin a ex ornare, lobortis enim maximus, congue sem. Nunc eu felis est. Fusce vitae ante et nisi tincidunt porta sit amet fringilla nulla. Cras convallis blandit dolor eu viverra. Phasellus gravida et lectus vel venenatis.
+        Cras ante est, ullamcorper in nulla in, consequat imperdiet velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam a ipsum non libero tristique posuere at ut lorem. Morbi ex nisl, molestie eu lectus nec, pellentesque facilisis nunc. Integer tempor purus congue ultrices sodales. Suspendisse non porttitor leo. Pellentesque dictum sem eu erat vulputate, ultricies molestie enim aliquam. Pellentesque ut diam a risus blandit sagittis. Fusce semper eros vel commodo cursus. Cras mollis ligula a metus aliquet, consequat molestie nibh posuere. Nulla luctus sollicitudin quam ac accumsan. Ut semper augue ac augue aliquam laoreet. Suspendisse maximus vulputate libero, eget venenatis dui sollicitudin in.
+        Suspendisse aliquet aliquam odio, vitae viverra magna aliquam sed. Sed sed felis egestas, blandit tellus quis, congue elit. Sed hendrerit ipsum et mollis bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam hendrerit orci orci, quis scelerisque velit gravida non. Duis non orci volutpat, sodales neque id, feugiat felis. Curabitur dignissim vitae eros ac maximus. Quisque ac varius ipsum, quis volutpat purus. Fusce a gravida dolor, sit amet hendrerit ligula. Nullam tortor purus, fringilla in maximus ac, sollicitudin eu nisi. Pellentesque nec magna magna. Cras rhoncus ligula nulla. Maecenas mattis pulvinar ante ut accumsan. Curabitur elementum arcu non nulla vulputate sagittis. Sed lectus arcu, ultrices nec velit sodales, dignissim iaculis justo.
+        Ut elementum, nulla quis volutpat faucibus, nunc dui consectetur nulla, quis tristique diam lectus in ipsum. Quisque elementum placerat erat ullamcorper facilisis. Proin arcu quam, luctus id magna a, scelerisque scelerisque quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed cursus lacus sit amet tellus volutpat, at auctor purus vulputate. Praesent id libero nec neque dignissim tempor. Mauris vitae nibh vitae magna commodo volutpat. Proin justo lectus, tincidunt sed condimentum et, cursus quis sapien. Aliquam at neque diam. Proin sed dignissim nulla. Nullam fringilla odio eget magna iaculis, nec dapibus magna congue. Nam ac quam sem. Duis vel bibendum nisi. Nam iaculis massa nec tellus cursus facilisis. Mauris id nulla fermentum, sodales lacus quis, sodales urna. Nullam et venenatis felis.
+        Aliquam hendrerit aliquam odio, ut pulvinar orci viverra non. Cras ornare rutrum risus ut hendrerit. Aenean nec congue nisi. Mauris nec mauris tincidunt, interdum ipsum nec, dictum elit. In eu quam sit amet neque lobortis egestas. Nam commodo quam felis, ac feugiat tellus facilisis sit amet. Integer neque arcu, condimentum ut varius volutpat, fermentum vel tortor. Quisque tempus nisi dui, bibendum scelerisque ipsum ultrices ut. Nunc feugiat sed mauris vel dapibus. Donec vitae sagittis nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus ac eros vel libero auctor rutrum. Nunc tempor tempus orci, sit amet fringilla ante pellentesque id.
+      </p>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -35,12 +35,15 @@ export default {
 }
 </script>
 <style scoped>
-    .title{
-        font-size: 50px;
-        color: #CDD1C4;
-    }
-    .subtitle{
-        font-size: 20px;
-        color: #CDD1C4;
-    }
+  .title{
+    font-size: 50px;
+    color: #000;
+  }
+  .subtitle{
+    font-size: 20px;
+    color: #000;
+  }
+  .b1, .b2, .b3{
+    margin-bottom: 20px;
+}
 </style>
