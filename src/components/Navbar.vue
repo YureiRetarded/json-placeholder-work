2c6ed9bce24e79462d1bee0b9b6aeecaabffda4d
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-05-28 16:54:06 +0300

    Navbar updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,22 @@
 <template>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
     <div class="container">
-        <div class="navbar-brand" @click="$router.push('/')">
-            <img src="@/assets/logo.svg" width="30" height="30">
-        </div>
-        <div class=" navbar-collapse">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <div @click="$router.push('/PostsPage')" class="nav-link active">Posts</div>
-        </li>
-        <li class="nav-item">
-          <div @click="$router.push('/UsersPage')" class="nav-link active">Users</div>
-        </li>
-      </ul>
-    </div>
+      <div class="navbar-brand" @click="$router.push('/')">
+          <img src="@/assets/logo.svg" width="30" height="30">
+      </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <div @click="$router.push('/PostsPage')" class="nav-link active">Posts</div>
+          </li>
+          <li class="nav-item">
+            <div @click="$router.push('/UsersPage')" class="nav-link active">Users</div>
+          </li>
+        </ul>
+      </div>
     </div>
 </nav>
 </template>
